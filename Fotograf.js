27b670ca4f8c5caed478577e2451751ad05cc337
Oleksii-Kramar <dp020991kos@gmail.com>
2022-12-20 21:09:31 +0100

    finish_commit

--- Fotograf.js
+++ Fotograf.js
@@ -1,53 +1,56 @@
-let page=1;
+let page = 1;
 
-async function getPhotos(){
-    const response=await fetch('https://picsum.photos/v2/list?page=1&limit=12')
-    const data=await response.json()
-    return data
+async function getPhotos(currentPage = 1) {
+  const response = await fetch(
+    `https://picsum.photos/v2/list?page=${currentPage}&limit=12`
+  );
+  const data = await response.json();
+  return data;
 }
-getPhotos().then(data=>{
-    const wrapper=document.querySelector(".wrapper")
-    data.forEach(el=>{
-        const div=document.createElement('div')
-        div.style.width="150px"
-        div.style.height="150px"
-const img = document.createElement('img')
-img.src=el.download_url
-div.appendChild(img)
-wrapper.appendChild(div)
-})
-})
+function renderImages(data) {
+  const wrapper = document.querySelector(".wrapper");
+  wrapper.innerHTML = "";
+  data.forEach((el) => {
+    const div = document.createElement("div");
+    div.style.width = "150px";
+    div.style.height = "150px";
+    const img = document.createElement("img");
+    img.src = el.download_url;
+    div.appendChild(img);
+    wrapper.appendChild(div);
+  });
+}
 
-const btnLeft = document.querySelector('#left')
-const btnRight = document.querySelector('#right')
+getPhotos().then((data) => {
+  renderImages(data);
+});
 
-btnLeft.addEventListener('click', () => {
-    console.log('click')
-})
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
 
-btnRight.addEventListener('click', () => {
-    // console.log('click')
-    window.location.href = 'http://127.0.0.1:5500/Fotograf.html'
-    getPhotos()
-})
+btnLeft.addEventListener("click", () => {
+  if (page <= 1) {
+    return;
+  }
 
+  page -= 1;
+  getPhotos(page).then((data) => {
+    renderImages(data);
+  });
+});
+
+btnRight.addEventListener("click", () => {
+  page += 1;
+  getPhotos(page).then((data) => {
+    renderImages(data);
+  });
+});
 
 // przyciski na dole strony w prawo w lewo
 
 // przycisk w prawo dodaje +1 do page i wywołuje getPhotos jeszcze raze
 
 // w lewo tak samo tylko -1
-
-
-
-
-
-
-
-
-
-
-
 
 // generator formularzy
 // const form=[
